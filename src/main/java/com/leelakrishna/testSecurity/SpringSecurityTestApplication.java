4ba11b3ae1914a1c3dd10f9483376c8f3445701d
unknown <leelakrishnacce@gmail.com>
2018-01-01 22:27:40 +0530

    add security test with userdefind database

---
+++ src/main/java/com/leelakrishna/testSecurity/SpringSecurityTestApplication.java
@@ -0,0 +1,12 @@
+package com.leelakrishna.testSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/leelakrishna/testSecurity/domain/UserRepository.java
@@ -0,0 +1,10 @@
+package com.leelakrishna.testSecurity.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    public User findByUserName(String username);
+    
+}
